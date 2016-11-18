ff6b6f166374e04b1a84687301044fdf358e85d9
CristianBoyaca <ccboyaca@misena.edu.co>
2016-11-17 21:13:21 -0500

    aa

---
+++ Inventario/Inventario/frmAdministrador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventario
+{
+    public partial class frmAdministrador : Form
+    {
+        public frmAdministrador()
+        {
+            InitializeComponent();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmLogin formLogin = new frmLogin();
+            formLogin.Show();
+            this.Hide();
+        }
+
+        private void registrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmRegistrarProveedor formProveedor = new frmRegistrarProveedor();
+            formProveedor.Show();
+        }
+    }
+}
